b25399997b93f0229c8faba0f4d2c5e315a1da90
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-03-12 09:50:45 -0700

    Fix toggle styling and add highlight for which toggle we clicked

--- dev/main.js
+++ dev/main.js
@@ -120,27 +120,58 @@ fetch("https://fisheyes.techkyra.com/getdata?col=genes")
 /* CHECK BOX FUNCTIONALITY */
 
 const cellCheckbox = document.getElementById("cellCheckbox");
-const geneCheckbox = document.getElementById('geneCheckbox');
+const geneRadioContainer = document.getElementById('geneRadioContainer');
+
+const toggleCellCheckbox = document.getElementById('toggleCellCheckbox');
+const toggleGeneRadio = document.getElementById('toggleGeneRadio');
 
 // Toggle the checkbox container visibility
-document.getElementById('toggleCellCheckbox').addEventListener('click', () => {
+toggleCellCheckbox.addEventListener('click', () => {
 
   cellCheckbox.style.display = cellCheckbox.style.display === 'none' ? 'block' : 'none';
 
+  // set to highlight mode
+  toggleCellCheckbox.style.backgroundColor = 'white';
+  toggleCellCheckbox.style.color = 'black';
+
+  // clear other toggle's style
+  toggleGeneRadio.style.backgroundColor = 'rgb(97, 97, 97)';
+  toggleGeneRadio.style.color = 'white';
+
   // check if gene checkbox is visible, if yes disable it
-  if (geneCheckbox.style.display === 'block') {
-    geneCheckbox.style.display = 'none';
+  if (geneRadioContainer.style.display === 'block') {
+    geneRadioContainer.style.display = 'none';
+  }
+
+  // reset it if clicked again
+  if (cellCheckbox.style.display === 'none') {
+    toggleCellCheckbox.style.backgroundColor = 'rgb(97, 97, 97)';
+    toggleCellCheckbox.style.color = 'white';
   }
 });
 
 // Toggle gene checkbox container
-document.getElementById('toggleGeneCelltype').addEventListener('click', () => {
-  const geneCheckbox = document.getElementById('geneCheckbox');
-  geneCheckbox.style.display = geneCheckbox.style.display === 'none' ? 'block' : 'none';
+toggleGeneRadio.addEventListener('click', () => {
+
+  geneRadioContainer.style.display = geneRadioContainer.style.display === 'none' ? 'block' : 'none';
+
+  // set to highlight mode
+  toggleGeneRadio.style.backgroundColor = 'white';
+  toggleGeneRadio.style.color = 'black';
+
+  // clear other toggle's style
+  toggleCellCheckbox.style.backgroundColor = 'rgb(97, 97, 97)';
+  toggleCellCheckbox.style.color = 'white';
 
   // check if cell checkbox is visible, if yes disable it
   if (cellCheckbox.style.display === 'block') {
     cellCheckbox.style.display = 'none';
+  }
+
+  // reset it if clicked again
+  if (geneRadioContainer.style.display === 'none') {
+    toggleGeneRadio.style.backgroundColor = 'rgb(97, 97, 97)';
+    toggleGeneRadio.style.color = 'white';
   }
 });
 
